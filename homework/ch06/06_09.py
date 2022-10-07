59a86a91693738e6dc269d1164681c4b74333a9f
minuminuchwichwi <minu2752@skuniv.ac.kr>
2022-10-07 15:49:58 +0900

    ADDED CHAPTER 6 HOMEWORKS.

--- homework/ch06/06_09.py
+++ homework/ch06/06_09.py
@@ -1,9 +1,17 @@
 import numpy as np, cv2
 
-def onChange(value):
+def onChange_alpha(value):
     global alpha, beta, title, image1, image2, dst
 
     alpha = cv2.getTrackbarPos('image1', title) / 100
+
+    image3 = cv2.addWeighted(image1, alpha, image2, beta, 0)
+    dst[0:h, w:w*2] = image3[0:h, 0:w]
+
+    cv2.imshow(title, dst)
+def onChange_beta(value):
+    global alpha, beta, title, image1, image2, dst
+
     beta = cv2.getTrackbarPos('image2', title) / 100
 
     image3 = cv2.addWeighted(image1, alpha, image2, beta, 0)
@@ -11,10 +19,10 @@ def onChange(value):
 
     cv2.imshow(title, dst)
 
+
 image1 = cv2.imread("add1.jpg", cv2.IMREAD_GRAYSCALE)
 image2 = cv2.imread("add2.jpg", cv2.IMREAD_GRAYSCALE)
-if image1 is None or image2 is None:
-    raise Exception("영상 파일 읽기 오류 발생")
+if image1 is None or image2 is None: raise Exception("영상 파일 읽기 오류 발생")
 
 title = 'dst'
 
@@ -22,14 +30,15 @@ alpha, beta = 0.5, 0.5
 image3 = cv2.addWeighted(image1, alpha, image2, beta, 0)
 
 w, h = image1.shape[:2]
+hor = np.zeros((w, h*2), np.uint8)
 dst = np.zeros((w, h*3), np.uint8)
-dst[0:h, 0:w] = image1[0:h, 0:w]
-dst[:, w*2:] = image2[0:h, 0:w]
-dst[0:h, w:w*2] = image3[0:h, 0:w]
+
+hor = cv2.hconcat([image1, image3])
+dst = cv2.hconcat([hor, image2])
 
 cv2.imshow(title, dst)
 
-cv2.createTrackbar('image1', title, 50, 100, onChange)
-cv2.createTrackbar('image2', title, 50, 100, onChange)
+cv2.createTrackbar('image1', title, 50, 100, onChange_alpha)
+cv2.createTrackbar('image2', title, 50, 100, onChange_beta)
 
 cv2.waitKey(0)
